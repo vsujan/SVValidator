9efb9f388933165f7c5dfc9b7f0b31b11e7fcd54
Sujan Vaidya <shujan1992@gmail.com>
2017-08-01 13:54:15 +0545

    Some fixes for carthage

---
+++ SVValidator/SVValidator.h
@@ -0,0 +1,19 @@
+//
+//  SVValidator.h
+//  SVValidator
+//
+//  Created by Sujan Vaidya on 8/1/17.
+//  Copyright © 2017 Sujan Vaidya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVValidator.
+FOUNDATION_EXPORT double SVValidatorVersionNumber;
+
+//! Project version string for SVValidator.
+FOUNDATION_EXPORT const unsigned char SVValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVValidator/PublicHeader.h>
+
+
